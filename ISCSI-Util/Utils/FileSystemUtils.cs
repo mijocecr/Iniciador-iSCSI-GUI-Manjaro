87450f8d5631de6a5d0c0f6ca733ac5d84cb0edf
Miguel Cerrato <miguel.cerrato.es@gmail.com>
2025-12-12 16:04:04 +0100

    Ajuste en funcion SanitizarNombre en FileSystemUtils

--- ISCSI-Util/Utils/FileSystemUtils.cs
+++ ISCSI-Util/Utils/FileSystemUtils.cs
@@ -1,4 +1,4 @@
-using System.IO;
+/*using System.IO;
 
 namespace ISCSI_Util.Utils;
 
@@ -25,6 +25,48 @@ public static class FileSystemUtils
 }
 
 
+
+public static class Credenciales
+{
+    public static string AdminPassword { get; set; }
+}
+*/
+
+
+using System.IO;
+
+namespace ISCSI_Util.Utils;
+
+public static class FileSystemUtils
+{
+    public static string CrearCarpetaMontaje(string iqn)
+    {
+        // 🔧 Cambiado: ahora la base es /home/iscsi
+        string basePath = "/home/iscsi";
+        string folderName = SanitizarNombre(iqn);
+        string fullPath = Path.Combine(basePath, folderName);
+
+        if (!Directory.Exists(fullPath))
+            Directory.CreateDirectory(fullPath);
+
+        return fullPath;
+    }
+
+    public static string SanitizarNombre(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return "iscsi";
+
+        // Reemplazar caracteres inválidos por '_'
+        foreach (var c in Path.GetInvalidFileNameChars())
+            input = input.Replace(c, '_');
+
+        // Reemplazar ':' explícitamente (Windows no lo permite en nombres de carpeta)
+        input = input.Replace(':', '_');
+
+        return input;
+    }
+}
 
 public static class Credenciales
 {
